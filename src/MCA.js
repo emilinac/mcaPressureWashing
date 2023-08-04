66fc2b9126cfc838a106c9cfeef0bc6508d54cd0
Emilina Sanduleac <e.codreanu@students.clark.edu>
2023-08-03 22:06:52 -0500

    added a couple lines stating what jobs are provided

--- src/MCA.js
+++ src/MCA.js
@@ -3,4 +3,4 @@
 // Description: A website for people to be able to look at before and after. The contact of the owner at the bottom of the page if they decide to book a service through them.
 
 
-//TODO
+//TODO services that are offered, before and after comparison, contact, social media, pricing.
